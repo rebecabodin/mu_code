47169514d3e4e2274a3e17631657ab0e63f965f6
rebecabodin <rebecabodin@gmail.com>
2025-08-29 20:21:48 -0300

    feat: implementação de novas funcionalidades

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.write ("Olá, mundo testando 1,2,3!")
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+price_max = df_top100_books["book price"].max ()
+price_min = df_top100_books["book price"].min ()
+
+
+max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
+df_books = df_top100_books[df_top100_books["book price"]<= max_price]
+df_books
