353dc73e331094e8f9cadc4183e63bd6b2498390
rebecabodin <rebecabodin@gmail.com>
2025-08-30 19:50:21 -0300

    feat: implementação de novas funcionalidades

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(layout="wide")
+
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
